7fedde58ad0fcee3584a8bf559469eec9111f4b0
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-02-19 15:01:11 +0000

    Added a list

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 def Name(x):
+    l = []
+    
     if type(x) == str:
         print(type(x))
+        for i in x:
+            l.append(i)
+        print(l)
     elif type(x) == list:
         print(type(x))
 
