f045ad2a02cbd85002412a10b2cb4419f7b68ad9
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-02-19 15:35:38 +0000

    Finished the str system.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+mi = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ma = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 def Name(x):
+    y = 0
+    nam = ''
     l = []
-    
+##
     if type(x) == str:
-        print(type(x))
         for i in x:
             l.append(i)
-        print(l)
+        for i in mi:
+            y += 1
+            if i == l[0]:
+                l[0] = str(ma[y - 1])
+        for i in l:
+            nam += i
+        return nam
     elif type(x) == list:
         print(type(x))
 
